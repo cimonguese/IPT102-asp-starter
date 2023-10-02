04def4f8d165496861f3d45a2ff85639e49b67d4
cimonguese <145822163+cimonguese@users.noreply.github.com>
2023-10-02 11:56:13 +0800

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,9 +6,9 @@ public class HomeController : Controller
     {
         var studentInfoArray = new[]
         {
-            new StudentInfoModel { Name = "John Doe", StudentId = "S001", StudentAge = "20" },
-            new StudentInfoModel { Name = "Jane Smith", StudentId = "S002", StudentAge = "22" },
-            new StudentInfoModel { Name = "Mike Johnson", StudentId = "S003", StudentAge = "31" }
+            new StudentInfoModel { Name = "Margaux Cimon Guese", StudentId = "19-1378", StudentAge = "23" },
+            new StudentInfoModel { Name = "Japhet Ruiz", StudentId = "21-1991", StudentAge = "20" },
+            new StudentInfoModel { Name = "John Partrick Patricio", StudentId = "19-0433", StudentAge = "23" }
         };
 
         return View(studentInfoArray);
